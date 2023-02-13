9db8fbb3057c28fac647d325e4b5c1eac3a86fb6
AndreiPetoukhov1986 <oziglena@mail.ru>
2023-02-13 23:44:08 +0300

    First commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Random;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int[] arr = {2,4,5,3,4,5,6,7,8,9};
+           for (int i = 0; i < arr.length; i++) {
+            if (arr[i] % 2 == 0) {
+                arr[i] += 1;
+            }
+            if (i >= 2 && i <= 6) {
+                System.out.println(arr[i]);
+            }
+        }
+
+    }
+}
+
+
